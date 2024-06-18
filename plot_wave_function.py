2afdef3c884fcc867e395da9f3de8ff9b0d4fa8d
Miguel Angel Ramírez Garrido <66762698+MikeRamirezGarrido@users.noreply.github.com>
2024-06-17 17:57:49 -0700

    Update plot_wave_function.py

--- plot_wave_function.py
+++ plot_wave_function.py
@@ -1,30 +1,27 @@
 #Hermit Purple by Ramirez Garrido
 #wave function ploting
 import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib import cbook
 
-f = open("/data.txt", 'rt')
+f = open("/content/drive/My Drive/Data/uni_5_data.txt", 'rt')
 data = f.readlines()
 x = []
-yh = []
+y = []
 p = []
 
 for line in data:
     values = line.split(",")
     x.append(float(values[0]))
-    yh.append(float(values[2]))
-    p.append(float(values[3]))
+    y.append(float(values[1]))
+    p.append(float(values[2]))
 
 plt.style.use('bmh')
-plt.plot(x, yh,'#177ba3', label='$\psi_n(x)$')
-plt.plot(x,p, '#7f059e', label='$|\psi_n(x)|^2$')
-plt.xlabel('$x$')
+plt.plot(x, y,'#7f059e', label='$\psi_n(x)$')
+plt.plot(x,p, '#fa7b05', label='$|\psi_n(x)|^2$')
+plt.xlabel('$x$  $[m]$')
 fig = plt.gcf()
 fig.set_facecolor('white')
 plt.legend(loc='upper left', labelcolor = 'linecolor')
 plt.title(label = 'Wave function n = 5', color = 'gray', fontstyle='italic', fontsize = 11.5)
 plt.tight_layout()
-!plt.savefig('schodingerCat3.7.png',  transparent=True, dpi=600)
-
+plt.savefig('unidim_5_harm.png' ,  transparent=True, dpi=600)
 plt.show()
