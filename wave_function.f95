e89fd3ced2a547a6f144155b8fcec4d7ff83130d
Miguel Angel Ramírez Garrido <66762698+MikeRamirezGarrido@users.noreply.github.com>
2024-06-17 18:46:48 -0700

    Update wave_function.f95

--- wave_function.f95
+++ wave_function.f95
@@ -1,3 +1,4 @@
+!Uni-dimesional quantum harmonic oscillator
 !Runge Kutta first function y' = w
        Function first(w)
               double precision, intent(IN) :: w
@@ -19,32 +20,28 @@
               double precision :: m1, m2, m3, m4
               double precision :: hbar, omega, masse, e, pi, xs, psi, T
               Write(*,*) "Hermit Purple by Ramirez Garrido"
+              Write(*,*) "Uni-dimesional quantum harmonic oscillator"
               Write(*,*) "Write initial values in source code"
-              x0 = -7
               pi = 3.141592654
               e = 2.718281828
-              
-
-              masse = 1
-              omega = 0.000115767
+!hbar using electron mass as mass unit               
               hbar = 0.000115767
-              
-              
-!Write here
-!Write degree
-              n = 5
-             
-!Write intial value H_n at x = 0     
-              y0 = -483784
-!Write intial value H_n' at x = 0,   H_{n}'(x) = 2*n*H_{n-1} (x) if that helps. 
-              w0 = 2*5*(36076)
+!Write here              
+              x0 = -7
+              masse = 1
+              omega = 0.00031
+              n = 8
+!Write intial value H_n at x = x0     
+              y0 = 1085747600
+!Write intial value H_n' at x = x0,   H_{n}'(x) = 2*n*H_{n-1} (x) if that helps. 
+              w0 = 2*8*(-83965616)
               T = 0
-              Open(3, file="data.txt")
+              Open(2, file="data.txt")
               xa = x0
               ya = y0
               wa = w0
               l = 1.0
-2             IF(l .LT. 14000.0) THEN           
+1             IF(l .LT. 14000.0) THEN           
               h = 0.001
               
               k1 = 0
@@ -58,27 +55,26 @@
               i = h/2
           
        
-	        k1 = first(wa)
+	       k1 = first(wa)
 		m1 = sec(n,xa,ya,wa)
-       	      	k2 = first(wa+m1*i)
-       	        m2 = sec(n,xa+i, ya+i*k1, wa+i*m1)
-       	        k3 = first(wa+m2*i)
-       	        m3 = sec(n,xa+i,ya+i*k2, wa+i*m2)
-       	        k4 = first(wa+m3*h)
-       	        m4 = sec(n,xa+h, ya+h*k3, wa+h*m3)
+       	k2 = first(wa+m1*i)
+       	m2 = sec(n,xa+i, ya+i*k1, wa+i*m1)
+       	k3 = first(wa+m2*i)
+       	m3 = sec(n,xa+i,ya+i*k2, wa+i*m2)
+       	k4 = first(wa+m3*h)
+       	m4 = sec(n,xa+h, ya+h*k3, wa+h*m3)
        	       
-                     xa = xa + h
-                     ya = ya + h*(k1+2.0*k2+2.0*k3+k4)/6.0
-                     wa = wa + h*(m1+2.0*m2+2.0*m3+m4)/6.0
-              
+              xa = xa + h
+              ya = ya + h*(k1+2.0*k2+2.0*k3+k4)/6.0
+              wa = wa + h*(m1+2.0*m2+2.0*m3+m4)/6.0
               	
               xs = SQRT(hbar/(masse*omega))*xa
               psi = ( 1/((2**n)*gamma(n+1))**0.5)*((masse*omega/(pi*hbar))**0.25)*(e**(-masse*omega*xs**2/(2*hbar)))
 	      
-              Write(3,*) xs,",", ya,",", ya*psi, ",", (ya*psi)**2
+              Write(2,*) xs, ",", ya*psi, ",", (ya*psi)**2
               T = T + ((ya*psi)**2)*xs*h/xa
               l = l + 1.0
-              GO TO 2
+              GO TO 1
               End If
               Write(*,*) T
        End Program
